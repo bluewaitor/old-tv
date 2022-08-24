5d8b13e3b1b7cebce8af68b22879daa922563d0e
blue <405622394@qq.com>
2022-08-24 09:32:51 +0800

    feat: update vite base

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  base: './',
   server: {
     proxy: {
         '/video': 'https://live2.kxm.xmtv.cn/dongdmt/playlist.m3u8',
